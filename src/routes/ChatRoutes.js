ae2ec079341b4c688f7869e58ee3dc417ded0d6a
vietreborn <viethovecamhung@gmail.com>
2026-05-23 18:30:37 +0700

    feat: update chat message routing

--- src/routes/ChatRoutes.js
+++ src/routes/ChatRoutes.js
@@ -4,26 +4,31 @@ const router = express.Router();
 
 const Message = require("../models/mongo/Message");
 
-router.post("/send", async (req, res) => {
+const authMiddleware = require("../middleware/authMiddleware");
 
-    try {
+router.post("/send", authMiddleware, async (req, res) => {
 
-        const { senderId, receiverId, message } = req.body;
+        try {
 
-        const newMessage = await Message.create({
-            senderId,
-            receiverId,
-            message
-        });
+            const { receiverId, message } = req.body;
 
-        res.status(201).json(newMessage);
+            const senderId = req.user.id;
 
-    } catch (error) {
+            const newMessage = await Message.create({
+                senderId,
+                receiverId,
+                message
+            });
 
-        res.status(500).json({
-            error: error.message
-        });
+            res.status(201).json(newMessage);
+
+        } catch (error) {
+
+            res.status(500).json({
+                error: error.message
+            });
+        }
     }
-});
+);
 
 module.exports = router;
